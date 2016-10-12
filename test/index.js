cf33ccfc958eb05ab8e218beec31a123ebeecc07
abhishek singh <baisabhishek64@gmail.com>
2016-10-12 23:10:06 +0530

    updating test

--- test/index.js
+++ test/index.js
@@ -1,7 +1,22 @@
+var request = require('request');
 describe('first suite',function(){
+
+    beforeEach(function(){
+       request('http://localhost:3000/start',function(error,res,body){
+           if(error)
+               console.log(error);
+       });
+    });
 
     it('first test',function(){
         console.log('testing!!!!');
     });
+
+    afterEach(function () {
+        request('http://localhost:3000/quit',function(error,res,body){
+           if(error)
+               console.log(error);
+        });
+    })
 
 });
