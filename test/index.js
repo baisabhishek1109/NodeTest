7ca9f52ae5b7421514270cbae62dc1e318c1fa69
abhishek singh <baisabhishek64@gmail.com>
2016-10-12 23:27:12 +0530

    adding file

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ var request = require('request');
 describe('first suite',function(){
 
     beforeEach(function(){
-        console.log('before each')
+        console.log('before each');
        request('http://192.168.0.9:3000/start',function(error,res,body){
            if(error)
                console.log(error);
@@ -21,6 +21,8 @@ describe('first suite',function(){
         request('http://192.168.0.9:3000/quit',function(error,res,body){
            if(error)
                console.log(error);
+
+            console.log('response');
 
             console.log(res);
             console.log(body);
